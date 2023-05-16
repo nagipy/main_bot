244f1cf5416298c9dd430ba55fe7ab011ea04572
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-05-16 19:21:49 +0900

    Update lobby.js

--- logs/lobby.js
+++ logs/lobby.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js")
 module.exports = client => {
   
 client.on("guildMemberAdd", async(member) => {
-  const channel = client.channels.cache.get(process.env.logs)
+  const channel = client.channels.cache.get(process.env.lobby)
 
   const embed = new MessageEmbed()
   .setColor("GREEN")
@@ -15,7 +15,7 @@ client.on("guildMemberAdd", async(member) => {
 })
 
 client.on("guildMemberRemove", async(member) => {
-  const channel = client.channels.cache.get(process.env.logs)
+  const channel = client.channels.cache.get(process.env.lobby)
 
   const embed = new MessageEmbed()
   .setColor("RED")
