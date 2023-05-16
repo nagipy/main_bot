d556d534c26383055bc9db400cd9bb440f285977
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-05-16 19:20:27 +0900

    Create lobby.js

---
+++ logs/lobby.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = client => {
+  
+client.on("guildMemberAdd", async(member) => {
+  const channel = client.channels.cache.get(process.env.logs)
+
+  const embed = new MessageEmbed()
+  .setColor("GREEN")
+  .setAuthor(memebr.displayName, member.user.displayAvatarURL({dynamic:true}))
+  .setDescription(`**${member.displayName}** has Joined\nID: ${member.id}\nUser createdAt: ${member.user.createdAt}`)
+  .setTimestamp()
+
+  channel.send(embed)
+})
+
+client.on("guildMemberRemove", async(member) => {
+  const channel = client.channels.cache.get(process.env.logs)
+
+  const embed = new MessageEmbed()
+  .setColor("RED")
+  .setAuthor(member.displayName, member.user.displayAvatarURL({dynamic:true}))
+  .setDescription(`**${member.displayName}** has Left\nID: ${member.id}`)
+
+  channel.send(embed)
+})
+  
+}
