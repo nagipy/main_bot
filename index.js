68c43a5664b1af89ef18a5e98891aa5be0e68891
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-06-11 15:51:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const client = new Client()
 
 client.on("ready", () => {
  console.log(`login ${client.user.tag}`)
- 
- // online(オンライン) idle(退席中) dnd(取り込み中) invisible(オフライン)
+
  client.user.setPresence({
   status: "online",
   activity: {
